673570a49b7c56db7db7643a83fa2f2ee30c1438
Remi Chuet <remschuet@gmail.5C5Ccom>
2022-06-02 19:13:39 -0400

    01

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import pygame
-from enemy import Enemy
-from player import Player
-from position_environment import PositionEnvironment
+from management_environment import ManagementEnvironment
 
 """
     REFACTORING
@@ -23,13 +21,6 @@ HUMAN_WIDTH = 80
 HUMAN_HEIGHT = 80
 HUMAN_SPEED = 2
 
-# create a list for the enemy
-enemy_list = []
-number_of_enemy = 0
-
-# create writing style
-arial_font = pygame.font.SysFont("arial", 20)
-
 # create root and the background root
 root = pygame.display.set_mode((ROOT_WIDTH, ROOT_HEIGHT))
 root_background_image = pygame.image.load("background_game.png")
@@ -43,9 +34,7 @@ clock = pygame.time.Clock()
 # every 2 seconds
 pygame.time.set_timer(pygame.USEREVENT, 2000)
 
-# Set up player
-position_environment = PositionEnvironment()
-player = Player(root, "player", "player", 400, 250, HUMAN_WIDTH, HUMAN_HEIGHT, HUMAN_SPEED, position_environment)
+management_environment = ManagementEnvironment(root, HUMAN_WIDTH, HUMAN_HEIGHT, HUMAN_SPEED, ROOT_WIDTH, ROOT_HEIGHT)
 
 launched = True
 while launched:
@@ -61,50 +50,23 @@ while launched:
             launched = False
         # for event every 2 sec
         elif event.type == pygame.USEREVENT:
-            number_of_enemy += 1
-            # create just 3 enemy
-            if number_of_enemy <= 3:
-                enemy_list.append(Enemy(root, "enemy", ("enemy"+str(number_of_enemy)), 50, 400,HUMAN_WIDTH,
-                                        HUMAN_HEIGHT, HUMAN_SPEED, position_environment))
+            management_environment.call_every_2_sec()
 
     # player mouvement
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and player.position_x > 0:
-        player.move_left()
-    if keys[pygame.K_RIGHT] and player.position_x < ROOT_WIDTH - HUMAN_WIDTH:
-        player.move_right()
-    if keys[pygame.K_UP] and player.position_y > 0:
-        player.move_up()
-    if keys[pygame.K_DOWN] and player.position_y < ROOT_HEIGHT - HUMAN_HEIGHT:
-        player.move_down()
-    if keys[pygame.K_1]:
-        # to can destroy an enemy
-        for enemy_item in enemy_list:
-            if isinstance(enemy_item, Enemy):
-                if enemy_item.name_id == "enemy1":
-                    enemy_list.remove(enemy_item)
-                    # destroy the position in position environment dict
-                    position_environment.destroy_enemy_in_list(enemy_item.name_id)
+    keys = pygame.key.get_pressed() # Pourrait servir à rediriger vers une fonction
+    if keys:
+        management_environment.key_pressed(keys)
+
 
     # mouvement enemy
-    for enemy_item in enemy_list:
-        if isinstance(enemy_item, Enemy):
-            enemy_item.movement()
+    management_environment.enemy_mouvement()
+
 
     # display the background image to the root
     root.blit(root_background_image, [0, 0])
 
     # draw enemy
-    for enemy_item in enemy_list:
-        if isinstance(enemy_item, Enemy):
-            enemy_item.draw()
-
-    # draw player
-    player.draw()
-
-    # display the fsp in the screen
-    fps_text = arial_font.render(f"{int(clock.get_fps())} fps", True, (0, 0, 0))
-    root.blit(fps_text, [(ROOT_WIDTH - 70), 20])
+    management_environment.draw_human_and_fps(clock)
 
     # main loop
     pygame.display.update()
--- management_environment.py
+++ management_environment.py
@@ -1,1 +1,67 @@
+import pygame
+from player import Player
+from enemy import Enemy
+from position_environment import PositionEnvironment
+
+
 class ManagementEnvironment:
+    def __init__(self, root, HUMAN_WIDTH, HUMAN_HEIGHT, HUMAN_SPEED, ROOT_WIDTH, ROOT_HEIGHT):
+        self.root = root
+        self.HUMAN_WIDTH = HUMAN_WIDTH
+        self.HUMAN_HEIGHT = HUMAN_HEIGHT
+        self.HUMAN_SPEED = HUMAN_SPEED
+        self.ROOT_WIDTH = ROOT_WIDTH
+        self.ROOT_HEIGHT = ROOT_HEIGHT
+
+        # create writing style
+        self.arial_font = pygame.font.SysFont("arial", 20)
+
+        # create a list for the enemy
+        self.enemy_list = []
+        self.number_of_enemy = 0
+
+        # Set up player
+        self.position_environment = PositionEnvironment()
+        self.player = Player(root, "player", "player", 400, 250,
+                             self.HUMAN_WIDTH, self.HUMAN_HEIGHT, self.HUMAN_SPEED, self.position_environment)
+
+    def call_every_2_sec(self):
+        self.number_of_enemy += 1
+        # create just 3 enemy
+        if self.number_of_enemy <= 3:
+            self.enemy_list.append(Enemy(self.root, "enemy", ("enemy" + str(self.number_of_enemy)), 50, 400, self.HUMAN_WIDTH,
+                                         self.HUMAN_HEIGHT, self.HUMAN_SPEED, self.position_environment))
+
+    def key_pressed(self, keys):
+        if keys[pygame.K_LEFT] and self.player.position_x > 0:
+            self.player.move_left()
+        if keys[pygame.K_RIGHT] and self.player.position_x < self.ROOT_WIDTH - self.HUMAN_WIDTH:
+            self.player.move_right()
+        if keys[pygame.K_UP] and self.player.position_y > 0:
+            self.player.move_up()
+        if keys[pygame.K_DOWN] and self.player.position_y < self.ROOT_HEIGHT - self.HUMAN_HEIGHT:
+            self.player.move_down()
+        if keys[pygame.K_1]:
+            # to can destroy an enemy
+            for enemy_item in self.enemy_list:
+                if isinstance(enemy_item, Enemy):
+                    if enemy_item.name_id == "enemy1":
+                        self.enemy_list.remove(enemy_item)
+                        # destroy the position in position environment dict
+                        self.position_environment.destroy_enemy_in_list(enemy_item.name_id)
+
+    def enemy_mouvement(self):
+        for enemy_item in self.enemy_list:
+            if isinstance(enemy_item, Enemy):
+                enemy_item.movement()
+
+    def draw_human_and_fps(self, clock):
+        # enemy
+        for enemy_item in self.enemy_list:
+            if isinstance(enemy_item, Enemy):
+                enemy_item.draw()
+        # player
+        self.player.draw()
+        # display the fsp in the screen
+        fps_text = self.arial_font.render(f"{int(clock.get_fps())} fps", True, (0, 0, 0))
+        self.root.blit(fps_text, [(self.ROOT_WIDTH - 70), 20])
